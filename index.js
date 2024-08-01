fb2f49372b3bba5e736c30710df6520102a5e73e
SARABJEET <sarabjeetsingh7316@gmail.com>
2024-08-01 16:23:37 +0530

    make some changes

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const { log } = require("console");
+
 const arr = [1, 2, 3];
+
+function ADD(arr) {
+  const sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
+}
+console.log(ADD(arr));
